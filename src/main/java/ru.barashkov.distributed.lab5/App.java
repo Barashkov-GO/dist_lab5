5b8cc4688f19838a330468427a0828a8d079e8fc
Barashkov-GO <gbarashkov2001@gmail.com>
2021-12-24 18:28:57 +0300

    Scripted auto-commit on change (2021-12-24 18:28:57) by gitwatch.sh

--- src/main/java/ru.barashkov.distributed.lab5/App.java
+++ src/main/java/ru.barashkov.distributed.lab5/App.java
@@ -2,6 +2,8 @@ package ru.barashkov.distributed.lab5;
 
 
 public class App {
+
+        
     public static void main(String[] args) throws IOException {
             System.out.println("start!");
             ActorSystem system = ActorSystem.create("routes");
