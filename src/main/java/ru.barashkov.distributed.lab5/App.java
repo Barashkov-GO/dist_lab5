dfd808c6b69b90e32a6f4cdaa6a1423ed4a2c682
Barashkov-GO <gbarashkov2001@gmail.com>
2021-12-24 21:57:43 +0300

    Scripted auto-commit on change (2021-12-24 21:57:43) by gitwatch.sh

--- src/main/java/ru.barashkov.distributed.lab5/App.java
+++ src/main/java/ru.barashkov.distributed.lab5/App.java
@@ -127,6 +127,7 @@ public class App {
                                                     sum -> new Pair<>(request.first(), sum / request.second())
                                             );
                                 }
+                            }
                         )
                     ).map(
                             result -> {
