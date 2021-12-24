a9502f354bde1d25647253f73f46c54eeba6f0d7
Barashkov-GO <gbarashkov2001@gmail.com>
2021-12-25 00:13:12 +0300

    Scripted auto-commit on change (2021-12-25 00:13:12) by gitwatch.sh

--- src/main/java/ru.barashkov.distributed.lab5/MessageGet.java
+++ src/main/java/ru.barashkov.distributed.lab5/MessageGet.java
@@ -1,7 +1,7 @@
 package ru.barashkov.distributed.lab5;
 
 public class MessageGet {
-    private String url;
+    private final String url;
 
     public MessageGet(String url){
         this.url = url;
