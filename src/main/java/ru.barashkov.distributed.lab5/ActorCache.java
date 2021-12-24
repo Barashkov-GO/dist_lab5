3c911d0459242983da038ce5b2e97ed78b6290e8
Barashkov-GO <gbarashkov2001@gmail.com>
2021-12-24 18:14:20 +0300

    Scripted auto-commit on change (2021-12-24 18:14:20) by gitwatch.sh

--- src/main/java/ru.barashkov.distributed.lab5/ActorCache.java
+++ src/main/java/ru.barashkov.distributed.lab5/ActorCache.java
@@ -6,7 +6,7 @@ public class ActorCache extends AbstractActor {
     private Map<String, Long> cache = new HashMap<>;
 
     public Recieve createRecieve() {
-        return RecieveBuilder
+        return RecieveBuilder.create().match()
 
     }
 
