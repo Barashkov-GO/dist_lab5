1dbc689af51226d47e496fb60a28380911b39a98
Barashkov-GO <gbarashkov2001@gmail.com>
2021-12-24 19:28:48 +0300

    Scripted auto-commit on change (2021-12-24 19:28:48) by gitwatch.sh

--- src/main/java/ru.barashkov.distributed.lab5/App.java
+++ src/main/java/ru.barashkov.distributed.lab5/App.java
@@ -76,7 +76,7 @@ public class App {
                                                     result)
                                             );
                                         } else {
-
+                                            
                                         }
                                 )
                     )
