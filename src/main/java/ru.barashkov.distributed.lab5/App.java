120f4f06193d72f14ab3e84b5a4605905ccd19dc
Barashkov-GO <gbarashkov2001@gmail.com>
2021-12-24 20:36:10 +0300

    Scripted auto-commit on change (2021-12-24 20:36:10) by gitwatch.sh

--- src/main/java/ru.barashkov.distributed.lab5/App.java
+++ src/main/java/ru.barashkov.distributed.lab5/App.java
@@ -112,7 +112,7 @@ public class App {
                                                     return resp.thenCompose(
                                                             req ->
                                                                     CompletableFuture.
-                                                                            completedFuture(System.currentTimeMillis() - begin)
+                                                                            completedFuture((int) System.currentTimeMillis() - begin)
 
                                                     );
                                                 }
