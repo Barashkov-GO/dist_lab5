7cdb22e91a78d181b63d4aced4d22b3648e56bf9
Barashkov-GO <gbarashkov2001@gmail.com>
2021-12-24 19:09:13 +0300

    Scripted auto-commit on change (2021-12-24 19:09:13) by gitwatch.sh

--- src/main/java/ru.barashkov.distributed.lab5/App.java
+++ src/main/java/ru.barashkov.distributed.lab5/App.java
@@ -25,6 +25,7 @@ public class App {
         public static void main(String[] args) throws IOException {
             System.out.println("start!");
             ActorSystem system = ActorSystem.create("routes");
+            ActorRef = 
             final Http http = Http.get(system);
             final ActorMaterializer materializer = ActorMaterializer.create(system);
             final Flow<HttpRequest, HttpResponse, NotUsed> routeFlow = func(http, system, materializer);
