168404af45d28c872c4366842bc0ac6b06a47d31
Barashkov-GO <gbarashkov2001@gmail.com>
2021-12-24 23:02:08 +0300

    Scripted auto-commit on change (2021-12-24 23:02:08) by gitwatch.sh

--- src/main/java/ru.barashkov.distributed.lab5/App.java
+++ src/main/java/ru.barashkov.distributed.lab5/App.java
@@ -91,7 +91,7 @@ public class App {
                                             Sink<Pair<String, Integer>, CompletionStage<Long>> testSink = Flow.
                                                     <Pair<String, Integer>>create().
                                                     mapConcat(
-                                                            r -> new ArrayList<>()
+                                                            r -> new ArrayList<>(Collections.)
 //                                                            {
 //                                                                ArrayList<String> out = new ArrayList<>();
 //                                                                for (int i = 0; i < r.second(); i++) {
