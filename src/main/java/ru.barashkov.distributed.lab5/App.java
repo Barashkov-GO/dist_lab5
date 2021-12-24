ff1f81deb1e462c565696ac8fa2fa3109a475d51
Barashkov-GO <gbarashkov2001@gmail.com>
2021-12-25 00:08:28 +0300

    Scripted auto-commit on change (2021-12-25 00:08:28) by gitwatch.sh

--- src/main/java/ru.barashkov.distributed.lab5/App.java
+++ src/main/java/ru.barashkov.distributed.lab5/App.java
@@ -33,7 +33,7 @@ import java.util.concurrent.CompletionStage;
 public class App {
         private static final String IP = "localhost";
         private static final String URL_PARAMETER = "testUrl";
-        private static final String COUNT_PARAMETER = "";
+        private static final String COUNT_PARAMETER = "count";
 
         private static final Integer PORT = 8080;
         private static final Integer PARALLELISM = 10;
@@ -45,7 +45,7 @@ public class App {
             ActorRef actorCache = system.actorOf(Props.create(ActorCache.class));
             final Http http = Http.get(system);
             final ActorMaterializer materializer = ActorMaterializer.create(system);
-            final Flow<HttpRequest, HttpResponse, NotUsed> routeFlow = func(http, system, materializer, actorCache);
+            final Flow<HttpRequest, HttpResponse, NotUsed> routeFlow = getFlow(http, system, materializer, actorCache);
             final CompletionStage<ServerBinding> binding = http.bindAndHandle(
                     routeFlow,
                     ConnectHttp.toHost(IP, PORT),
@@ -58,7 +58,7 @@ public class App {
                     .thenAccept(unbound -> system.terminate());
         }
 
-        public static Flow<HttpRequest, HttpResponse, NotUsed> func(
+        public static Flow<HttpRequest, HttpResponse, NotUsed> getFlow(
                 Http http,
                 ActorSystem system,
                 ActorMaterializer materializer,
@@ -67,8 +67,8 @@ public class App {
                     map(
                             request -> {
                                 Query q = request.getUri().query();
-                                String url = q.get("url").get();
-                                Integer count = Integer.parseInt(q.get("count").get());
+                                String url = q.get(URL_PARAMETER).get();
+                                Integer count = Integer.parseInt(q.get(COUNT_PARAMETER).get());
                                 return new Pair<>(url, count);
                             }
                     ).
