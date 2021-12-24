e67f6a5a0f74adf13d9d671f8b02b4229635c811
Barashkov-GO <gbarashkov2001@gmail.com>
2021-12-24 20:50:34 +0300

    Scripted auto-commit on change (2021-12-24 20:50:34) by gitwatch.sh

--- src/main/java/ru.barashkov.distributed.lab5/App.java
+++ src/main/java/ru.barashkov.distributed.lab5/App.java
@@ -132,7 +132,7 @@ public class App {
                                         ),
                                         ActorRef.noSender()
                                 );
-                                return HttpResponse.create().withEntity(result.first() + " - " + result.second())
+                                return HttpResponse.create().withEntity((result.first() + " - " + result.second()).toString)
 
         }
 
