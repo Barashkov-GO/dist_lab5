e53868d79fbc94aefe45db98244aaf429ffc528a
Barashkov-GO <gbarashkov2001@gmail.com>
2021-12-24 21:56:01 +0300

    Scripted auto-commit on change (2021-12-24 21:56:01) by gitwatch.sh

--- src/main/java/ru.barashkov.distributed.lab5/App.java
+++ src/main/java/ru.barashkov.distributed.lab5/App.java
@@ -124,7 +124,7 @@ public class App {
                                             toMat(testSink, Keep.right()).
                                             run(materializer).
                                             thenApply(
-                                                    sum -> new Pair<>(request.first(), )
+                                                    sum -> new Pair<>(request.first(), sum / request.second())
                                             );
                                 }
                             }
