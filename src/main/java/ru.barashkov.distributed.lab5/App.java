489857034319ff2d7b89ed12ca18494b06a8d3d2
Barashkov-GO <gbarashkov2001@gmail.com>
2021-12-24 21:30:21 +0300

    Scripted auto-commit on change (2021-12-24 21:30:21) by gitwatch.sh

--- src/main/java/ru.barashkov.distributed.lab5/App.java
+++ src/main/java/ru.barashkov.distributed.lab5/App.java
@@ -78,7 +78,7 @@ public class App {
                                         TIMEOUT
                     ).thenCompose(
                         result -> {
-                            if ((Optional<Long>)result) {
+                            if (((Optional<Long>) result).isPresent()) {
                                 return CompletableFuture.completedFuture(
                                         new Pair<String, Long>(
                                                 m.first(),
